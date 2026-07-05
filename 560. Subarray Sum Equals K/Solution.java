a2804041816382faf072a1105d56f28f1f587aa3
Aman Singh <amxnsingh07@gmail.com>
2026-07-05 21:54:34 +0530

    560. Subarray Sum Equals K

---
+++ 560. Subarray Sum Equals K/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+
+// static {
+// Runtime.getRuntime().addShutdownHook (new Thread(() -> {
+// try (FileWriter fw = new FileWriter("display_runtime.txt")) {
+// fw.write("0");
+// } catch (Exception e) {}
+// }));
+// }
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int count = 0;
+        for(int i=0;i<n;i++){
+            int sum = 0;
+            for(int j=i;j<n;j++){
+                sum+=nums[j];
+                if(sum==k){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
