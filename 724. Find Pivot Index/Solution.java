521411d10c3389bb902d985415ae5f5bb7350809
Aman Singh <amxnsingh07@gmail.com>
2026-07-06 22:36:33 +0530

    .

--- 724. Find Pivot Index/Solution.java
+++ 724. Find Pivot Index/Solution.java
@@ -7,7 +7,7 @@ class Solution {
        int leftsum = 0;
        for(int i =0;i<nums.length;i++){
         // int rightsum = totalsum-leftsum-nums[i];
-        // pivot means leftsum==rightsum so that means int rightsum = totalsum-leftsum-nums[i]; this line becomes int leftsum = totalsum-leftsum-nums[i]; so combinig both leftsum it becomes 2*leftsum;
+        //  pivot means leftsum==rightsum so that means int rightsum = totalsum-leftsum-nums[i]; this line becomes int leftsum = totalsum-leftsum-nums[i]; so combinig both leftsum it becomes 2*leftsum;
         if(2*leftsum == totalsum-nums[i]) return i;
         leftsum+=nums[i];
     
