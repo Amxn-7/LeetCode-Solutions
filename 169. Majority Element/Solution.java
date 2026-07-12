ceef22e89a229faa2f45f0afcdf7bc579e882111
Aman Singh <amxnsingh07@gmail.com>
2026-07-12 18:14:26 +0530

    169. Majority Element

---
+++ 169. Majority Element/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n  = nums.length;
+        int pp = 0;
+        int count = 0;
+        for(int i = 0;i<n;i++){
+            if(count==0) {
+                pp = nums[i];
+            }
+            if(nums[i] == pp){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return pp;
+    }
+}
